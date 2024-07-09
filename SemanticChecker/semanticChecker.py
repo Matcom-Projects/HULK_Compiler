dd2f1f22a3fd400d98ded6205faa2a95b2a9e073
Alejandro Ramírez Trueba <alejandrito110402@gmail.com>
2024-07-08 20:12:49 -0400

    update

--- SemanticChecker/semanticChecker.py
+++ SemanticChecker/semanticChecker.py
@@ -1,4 +1,4 @@
-from AST.ast import *
+from hulk_ast.ast_nodes import *
 import cmp.visitor as visitor
 from cmp.semantic import Scope
 from cmp.semantic import SemanticError
@@ -18,7 +18,7 @@ class TypeCollector(object):
     def __init__(self, errors=[]):
         self.context = None
         self.errors = errors
-    
+
     @visitor.on('node')
     def visit(self, node):
         pass
@@ -37,22 +37,22 @@ class TypeCollector(object):
         iterable_protocol=self.context.create_protocol('Iterable')
         iterable_protocol.define_method('next',[],[],self.context.get_type('Boolean'))
         iterable_protocol.define_method('current',[],[],self.context.get_type('Object'))
-        
+
         for decl in node.decl_list:
             self.visit(decl)
         return self.context
-        
+
     @visitor.when(TypeDeclNode)
     def visit(self, node: TypeDeclNode):
         try :
             self.context.create_type(node.id)
         except SemanticError as e:
             self.errors.append(e)
-            
+
     @visitor.when(ProtDeclNode)
     def visit(self, node:ProtDeclNode):
         try :
-            
+
             self.context.create_protocol(node.id)
         except SemanticError as e:
             self.errors.append(e)
@@ -63,7 +63,7 @@ class TypeBuilder:
         self.context = context
         self.current_type: Type = None
         self.errors = errors
-    
+
     @visitor.on('node')
     def visit(self, node):
         pass
@@ -74,30 +74,30 @@ class TypeBuilder:
                 self.visit(decl)
             except SemanticError as error:
                 self.errors.append(error)
-    
+
     @visitor.when(TypeDeclNode)
     def visit(self, node: TypeDeclNode):
         self.current_type = self.context.get_type(node.id)
         self.current_type.set_parent(self.context.get_type(node.parent) if node.parent != None else self.context.get_type("Object"))
         type_args=[]
 
-            
+
         for arg in node.args:
             actual_type=self.context.get_type(arg.type) if arg.type!=None else self.context.get_type('Object')
             type_args.append(VariableInfo(arg.id,actual_type))
         self.current_type.args=type_args
-        
+
 
         for feature in node.features:
             self.visit(feature)
-        
+
         self.current_type=None
-    
+
     @visitor.when(AssignNode)
-    def visit(self, node: AssignNode): 
-        type_node = self.context.get_type(node.var.type) if node.var.type!=None else self.context.get_type("Object") 
+    def visit(self, node: AssignNode):
+        type_node = self.context.get_type(node.var.type) if node.var.type!=None else self.context.get_type("Object")
         self.current_type.define_attribute(node.var.id,type_node)
-        
+
     @visitor.when(MethodNode)
     def visit(self, node: MethodNode):
         type=self.context.get_type(node.return_type if node.return_type!=None else 'Object')
@@ -108,15 +108,15 @@ class TypeBuilder:
             param_names.append(aux.id)
             param_types.append(self.context.get_type(aux.type if aux.type != None else "Object"))
         self.current_type.define_method(node.id,param_names,param_types, type)
-    
+
     @visitor.when(ProtDeclNode)
     def visit(self, node: ProtDeclNode):
         self.current_type= self.context.get_protocol(node.id)
         self.current_type.set_parent(self.context.get_type(node.parents) if node.parents!=None else self.context.get_type("Object"))
         for method in node.methods:
             self.visit(method)
-    
-    
+
+
     @visitor.when(ProtMethodNode)
     def visit(self, node: ProtMethodNode):
         type=self.context.get_type(node.return_type if node.return_type!=None else 'Object')
@@ -127,7 +127,7 @@ class TypeBuilder:
             param_names.append(aux.id)
             param_types.append(self.context.get_type(aux.type if aux.type != None else "Object"))
         self.current_type.define_method(node.id,param_names,param_types,type)
-    
+
     @visitor.when(FuncDeclNode)
     def visit(self, node: FuncDeclNode):
         #las funciones seran metodos de tipo "Function"
@@ -140,7 +140,7 @@ class TypeBuilder:
             param_names.append(aux.id)
             param_types.append(self.context.get_type(aux.type if aux.type != None else "Object"))
         function_type.define_method(node.id,param_names,param_types,return_type)
-            
+
 
 
 class TypeChecker:
@@ -159,63 +159,63 @@ class TypeChecker:
         newScope=Scope()
         for decl in node.decl_list:
             self.visit(decl,newScope)
-   
+
     @visitor.when(TypeDeclNode)
     def visit(self, node: TypeDeclNode, scope:Scope):
         self.current_type=self.context.get_type(node.id)
-        
+
         childScope=scope.create_child()
         childScope.define_variable('self',self.current_type)
         for arg in node.args:
             self.visit(arg,childScope)
-        
+
         for feature in node.features:
             self.visit(feature,childScope)
-        
+
         self.current_type=None
-    
+
     @visitor.when(AssignNode)
     def visit(self, node: AssignNode, scope:Scope):
-        
+
         type:Type=self.visit(node.var,scope)
         expr_type:Type=self.visit(node.expr,scope)
-        
+
         if(not expr_type.conforms_to(type)):
-            self.errors.append(SemanticError(INCOMPATIBLE_TYPES%(type.name,expr_type.name))) 
+            self.errors.append(SemanticError(INCOMPATIBLE_TYPES%(type.name,expr_type.name)))
             raise
-        
+
         if(self.current_method!=None and self.current_method==None):
             return
-        
+
 
         return expr_type
-        
+
     @visitor.when(MethodNode)
     def visit(self, node: MethodNode, scope:Scope):
         actual_method:Method=self.current_type.get_method(node.id)
         new_child=scope.create_child()
-        
-        #Declarando como variables los parametros de la funcion en el 
+
+        #Declarando como variables los parametros de la funcion en el
         #nuevo scope
         for i in range(0,len(actual_method.param_names)):
             new_child.define_variable(actual_method.param_names[i],
                                       actual_method.param_types[i])
-        
+
         self.current_method=actual_method
         expr_type= self.visit(node.body,new_child)
         self.current_method=None
-        
+
         if(not expr_type.conforms_to(node.return_type)):
             print(f'Problema con los tipos en metodo "{actual_method.name}"')
             self.errors.append(f'Problema con los tipos en metodo "{actual_method.name}"')
-    
+
     @visitor.when(ExprBlockNode)
     def visit(self, node: ExprBlockNode, scope:Scope):
         expr_return_type=self.context.get_type('Object')
         for expr in node.expr_list:
             expr_return_type=self.visit(expr,scope)
         return expr_return_type
-    
+
     @visitor.when(LetNode)
     def visit(self, node: LetNode, scope:Scope):
         new_scope=scope.create_child()
@@ -223,8 +223,8 @@ class TypeChecker:
             self.visit(assign,new_scope)
         type_expr=self.visit(node.expr,new_scope)
         return type_expr
-    
-    
+
+
     @visitor.when(WhileNode)
     def visit(self, node: WhileNode, scope:Scope):
         expr_type:Type=self.visit(node.cond,scope)
@@ -234,36 +234,36 @@ class TypeChecker:
         new_scope=scope.create_child()
         while_block_type= self.visit(node.body,new_scope)
         return while_block_type
-    
+
     @visitor.when(ForNode)
     def visit(self, node: ForNode, scope:Scope):
-        
-        #magia con la expresion para ver si iterable 
-        
+
+        #magia con la expresion para ver si iterable
+
         new_scope =scope.create_child()
         expr_type:Type =self.visit(node.iterable,scope)
-        
+
         #chequeando que implemente los metodos necesarios para ser iterable
         next_func:Method= expr_type.get_method('next')
         current_func:Method= expr_type.get_method('current')
         if(next_func.return_type.name!='Boolean'):
-            self.errors.append(f'Ninguna no es iterable este for')    
+            self.errors.append(f'Ninguna no es iterable este for')
         new_scope.define_variable( node.id, self.context.get_type('Number'))
-        
-        
+
+
         return self.visit(node.body,new_scope)
-    
+
     @visitor.when(DestrAssign)
     def visit(self, node: DestrAssign, scope:Scope):
-        
-        
+
+
         variable_type=self.visit(node.id)
         type:Type=self.visit(node.expr,scope)
         if(not type.conforms_to(variable_type)):
             self.errors.append(INCOMPATIBLE_TYPES)
             raise
         return variable_type
-    
+
     @visitor.when(MethodCallNode)
     def visit(self, node: MethodCallNode, scope:Scope):
         obj_type = self.visit(node.obj,scope)
@@ -281,21 +281,21 @@ class TypeChecker:
                 self.errors.append(f'Tipo de argumento erroneo')
                 raise
         return actual_method.return_type
-    
+
     @visitor.when(AttrrCallNode)
     def visit(self ,node : AttrrCallNode, scope:Scope):
-        
-        
+
+
 
         instance_type:Type = self.visit(node.obj,scope)
         atrribute_in_type:Attribute =instance_type.get_attribute(node.id)
         return atrribute_in_type.type
-        
-        
-    
+
+
+
     @visitor.when(FuncCallNode)
     def visit(self ,node : FuncCallNode, scope:Scope):
-        
+
         #tipo donde guardo las funciones globales
         function_type:Type =self.context.get_type('Function')
         actual_function:Method =function_type.get_method(node.id)
@@ -312,23 +312,23 @@ class TypeChecker:
                 self.errors.append(f'Tipo de argumento erroneo')
                 raise
         return actual_function.return_type
-    
+
     @visitor.when(InstantiateNode)
     def visit(self ,node : InstantiateNode, scope:Scope):
-        
+
         type:Type=self.context.get_type(node.type)
         args_types:list[Type]=[]
         for arg in node.expr_list:
             actual_type=self.visit(arg,scope)
             args_types.append(actual_type)
-        
+
         if(len(args_types)!=len(type.args)):
             self.errors.append(f'Incongruencia en cantidad de argumentos')
         for i in range(len(type.args)):
            if not args_types[i].conforms_to(type.args[i].type):
                self.errors.append(f'Incongruencia en tipo de argumentos')
         return type
-    
+
     @visitor.when(VectorNode)
     def visit(self ,node : VectorNode, scope:Scope):
         last_type:Type=self.context.get_type('Object');
@@ -339,18 +339,18 @@ class TypeChecker:
                 raise
         vector_type:Type=self.context.get_type('Vector')
         vector_type.define_method('current',[],[],last_type)
-        
+
         return vector_type
-    
+
     @visitor.when(DowncastNode)
     def visit(self, node:DowncastNode, scope:Scope):
-        
+
         cast_type=self.context.get_type(node.type)
         instance_type=self.visit(node.obj,scope)
         if(not instance_type.conforms_to(cast_type)):
             self.errors.append(f'Intento erroneo de casteo')
         return cast_type
-    
+
     @visitor.when(IndexingNode)
     def visit(self, node:IndexingNode, scope:Scope):
         type:Type=self.visit(node.vector,scope)
@@ -360,7 +360,7 @@ class TypeChecker:
             raise
         method_to_get_type:Method=type.get_method('current')
         return method_to_get_type.return_type
-    
+
     @visitor.when(VarNode)
     def visit(self, node:VarNode, scope:Scope):
         if not scope.is_defined(node.lex):
@@ -368,19 +368,19 @@ class TypeChecker:
             raise
         var =scope.find_variable(node.lex)
         return var.type
-    
+
     @visitor.when(VarDefNode)
     def visit(self, node:VarDefNode, scope:Scope):
-        
+
         if scope.is_local(node.id):
             self.errors.append(f'Variable {node.id} ya definidia')
             raise
         var_type=self.context.get_type(node.type);
         scope.define_variable(node.id,var_type)
         return var_type
-    
-    
-        
+
+
+
 def implements_protocol(protocol:Type,tipo:Type):
     for method in list(protocol.all_methods()):
         try :tipo.get_method(method[0].name)
